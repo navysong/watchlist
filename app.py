0bb69f37bbcddb728d9b975a456899771e783298
宋海军 <songhaijun1020@gmail.com>
2019-11-24 17:26:41 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
-def hello():	
-	return 'Welcome to My Watchlist!'
+def index():	
+	return render_template('index.html', name=name, movies=movies)
+	
+name = 'navy Song'
+movies = [
+	{'title':'肖申克的救赎The Shawshank Redemption', 'year': '1994'},
+	{'title':'控方证人witness for the Prosecution', 'year': '1957'},
+	{'title':'这个杀手不太冷', 'year': '1994'},
+	{'title':'霸王别姬', 'year': '1993'},
+	{'title':'美丽人生', 'year': '1997'},
+	{'title':'阿甘正传Forrest Gump', 'year': '1994'},
+	{'title':'辛德勒的名单Schindler\'s List', 'year': '1993'},
+	{'title':'教父The Godfather', 'year': '1972'},
+	]
